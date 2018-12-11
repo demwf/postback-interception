a4add4cab9382365148bea4a4c3c548ab8d85b3c
demwf <demwf@yandex.ru>
2018-12-11 17:23:37 +0500

    settings

--- private.php
+++ private.php
@@ -1,8 +1,8 @@
 <?php
 $HOST = 'localhost';
-$USER = 'demwf_tracker';
-$PASS = 'BuratinO755_';
-$BD = 'demwf_tracker';
+$USER = '###';
+$PASS = '###';
+$BD = '###';
 $DOMAIN_REDIRECT = 'http://trp.2amazing.ru/track/p.php?';
 $ADDITIONAL_UTM = 'n=custom&ak=c36e8e7b64&txt_param20=USD';
 ?>
